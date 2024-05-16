ae1d5c4c945252def84e0daaeea3394fdb5aae0e
Parthinsomniacs <parth.makwana@insomniacs.in>
2024-05-16 09:48:24 +0000

    datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; //new JS code version
+
+// alert(3+3) //we are uisng nodejs,not in browser
+
+//datatype
+
+// number
+//bigint
+//string
+//boolean
+//null
+// undefined
+
+//object
+
---
+++ 01_basic/test.js
@@ -0,0 +1,13 @@
+const accoubtId = 1235
+let fullName = "Parth"
+var accPass ="5454"
+accountCity="bhavnagar"
+
+let accountState;
+
+accPass ="33231"
+fullName ="khushi"
+accountCity = "Surat"
+//  accoubtId = 58955
+ console.log(accPass);
+ console.table([accPass,fullName,accountCity,accountState]);
